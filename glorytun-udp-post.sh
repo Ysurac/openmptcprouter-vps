42c7489c23627a87efbabe4775d71918a5f023dd
Ycarus (Yannick Chabanois) <ycarus@zugaina.org>
2020-04-07 16:06:54 +0200

    Set VPN IP only when possible

--- glorytun-udp-post.sh
+++ glorytun-udp-post.sh
@@ -4,5 +4,7 @@
 INTF=gt-udp-${DEV}
 [ -z "$LOCALIP" ] && LOCALIP="10.255.254.1"
 [ -z "$BROADCASTIP" ] && BROADCASTIP="10.255.254.3"
-ip link set dev ${INTF} up 2>&1 >/dev/null
-ip addr add ${LOCALIP}/30 brd ${BROADCASTIP} dev ${INTF} 2>&1 >/dev/null
+[ "$(ip addr show dev $INTF | grep -o 'inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*' | grep -o '[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*')" != "$LOCALIP" ] && {
+	ip link set dev ${INTF} up 2>&1 >/dev/null
+	ip addr add ${LOCALIP}/30 brd ${BROADCASTIP} dev ${INTF} 2>&1 >/dev/null
+}

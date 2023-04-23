56d3c8ae0826a562ac00f83bb6e66a3a0a3323fa
Ycarus (Yannick Chabanois) <ycarus@zugaina.org>
2023-04-23 05:40:20 +0000

    Fix fw patch

--- debian9-x86_64.sh
+++ debian9-x86_64.sh
@@ -1183,7 +1183,7 @@ if [ "$DSVPN" = "yes" ]; then
 		git clone https://github.com/jedisct1/dsvpn.git /tmp/dsvpn
 		cd /tmp/dsvpn
 		git checkout ${DSVPN_VERSION}
-		wget https://github.com/Ysurac/openmptcprouter-feeds/raw/develop/dsvpn/patches/nofirewall.patch
+		wget https://github.com/Ysurac/openmptcprouter-feeds/raw/956d791ea49d63e3e5b18cac510b53987c3f1a36/dsvpn/patches/nofirewall.patch
 		patch -p1 < nofirewall.patch
 		make CFLAGS='-DNO_DEFAULT_ROUTES -DNO_DEFAULT_FIREWALL'
 		make install

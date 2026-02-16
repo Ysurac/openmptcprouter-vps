2165545964cecad82a9e268b7627675cec2d3d31
Ycarus (Yannick Chabanois) <ycarus@zugaina.org>
2026-02-16 08:31:32 +0000

    v0.1050-rolling

--- debian9-x86_64.sh
+++ debian9-x86_64.sh
@@ -110,7 +110,7 @@ VPSURL="https://www.openmptcprouter.com/"
 REPO="repo.openmptcprouter.com"
 CHINA=${CHINA:-no}
 
-OMR_VERSION="0.1049-rolling-test"
+OMR_VERSION="0.1050-rolling-test"
 
 DIR=$( pwd )
 #"
